69bd0cdb35491039659c1c5812fc4f42c8425630
fwright12 <flintw11@gmail.com>
2024-12-22 13:08:33 -0500

    Move and reorganize styles

---
+++ Movies/Resources/Styles/Constants.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Movies
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Constants : ResourceDictionary
+    {
+        public const double TwoThirds = 2d / 3d;
+
+        public Constants()
+        {
+            InitializeComponent();
+        }
+    }
+}
