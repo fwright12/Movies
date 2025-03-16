643f5f337bca142c26c465e280882078546ec491
fwright12 <flintw11@gmail.com>
2025-03-16 16:29:56 -0400

    Add MappingConverter for Dictionary based mappings

---
+++ Movies/Converters/CornerRadiusToThicknessConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies
+{
+    public class CornerRadiusToThicknessConverter : IValueConverter<CornerRadius, Thickness>
+    {
+        public static readonly CornerRadiusToThicknessConverter Instance = new CornerRadiusToThicknessConverter();
+
+        public object? Convert(CornerRadius value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return new Thickness(
+                Math.Max(value.BottomLeft, value.TopLeft),
+                Math.Max(value.TopLeft, value.TopRight),
+                Math.Max(value.TopRight, value.BottomRight),
+                Math.Max(value.BottomRight, value.BottomLeft)
+                );
+        }
+
+        public object? ConvertBack(Thickness value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
